ce20c18163a6274e581fe8cf28c0ae0bf832a52b
Federica Bogo <fbogo@meta.com>
2026-04-22 02:14:48 -0700

    Add --body-model and --smpl-model-path CLI options
    Reviewed By: SummerIcequeen
    
    Differential Revision: D98126181
    
    fbshipit-source-id: b4869ba09a93c78d7c999aafb1a0adb6771c875f

--- viewer.py
+++ viewer.py
@@ -12,6 +12,7 @@ import rerun as rr
 from loguru import logger
 from nymeria.data_provider import NymeriaDataProvider
 from nymeria.data_viewer import NymeriaViewer
+from nymeria.definitions import BodyModel
 
 
 @click.command()
@@ -21,7 +22,23 @@ from nymeria.data_viewer import NymeriaViewer
 @click.option(
     "-s", "save_rrd", is_flag=True, default=False, help="Save rerun into logfile"
 )
-def main(sequence_dir: Path, save_rrd: bool) -> None:
+@click.option(
+    "--body-model",
+    "body_model",
+    type=click.Choice(["momentum", "smpl", "mhr"], case_sensitive=False),
+    default="momentum",
+    help="Body model to use: momentum (default), smpl, or mhr",
+)
+@click.option(
+    "--smpl-model-path",
+    "smpl_model_path",
+    type=Path,
+    default=None,
+    help="Path to SMPL model .pkl file (required when --body-model=smpl)",
+)
+def main(
+    sequence_dir: Path, save_rrd: bool, body_model: str, smpl_model_path: Path
+) -> None:
     logger.remove()
     logger.add(
         sys.stdout,
@@ -30,8 +47,16 @@ def main(sequence_dir: Path, save_rrd: bool) -> None:
         level="INFO",
     )
 
+    if body_model == "smpl" and smpl_model_path is None:
+        raise click.UsageError("--smpl-model-path is required when --body-model=smpl")
+
     # See NymeriaDataProviderConfig for configuration
-    nymeria_dp = NymeriaDataProvider(sequence_rootdir=sequence_dir, load_wrist=True)
+    nymeria_dp = NymeriaDataProvider(
+        sequence_rootdir=sequence_dir,
+        load_wrist=True,
+        body_model=BodyModel(body_model),
+        smpl_model_path=str(smpl_model_path) if smpl_model_path else None,
+    )
 
     output_rrd: Path = sequence_dir / "nymeria.rrd" if save_rrd else None
     viewer = NymeriaViewer(output_rrd=output_rrd)
